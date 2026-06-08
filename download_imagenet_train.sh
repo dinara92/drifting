94ab4dceb95b28e30fc8ffc227d25a33c106dc11
Dinara Aliyeva <dinara.ali@gmail.com>
2026-06-08 13:22:18 -0400

    imagenet script

---
+++ download_imagenet_train.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=train_imagenet_download
+#SBATCH --partition=all
+#SBATCH --time=12:00:00
+#SBATCH --mem=4G
+#SBATCH --cpus-per-task=2
+#SBATCH --output=/playpen-shared/dinara/datasets/imagenet/download_%j.log
+
+mkdir -p /playpen-shared/dinara/datasets/imagenet
+
+wget --user=adinara --password=xl1-2-3-4-5! \
+  https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_train.tar \
+  -P /playpen-shared/dinara/datasets/imagenet/
+
+# wget --user=adinara --password=xl1-2-3-4-5! \
+#   https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_val.tar \
+#   -P /playpen-shared/dinara/datasets/imagenet/
