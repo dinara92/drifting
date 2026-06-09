1c23e890c7e0fc961f5ca95d521f202ab57cb4ec
Dinara Aliyeva <dinara.ali@gmail.com>
2026-06-09 13:53:28 -0400

    env file

--- utils/env.py
+++ utils/env.py
@@ -4,10 +4,18 @@ from __future__ import annotations
 
 import os
 
-IMAGENET_PATH = "/path/to/imagenet"
+# IMAGENET_PATH = "/path/to/imagenet"
+# IMAGENET_CACHE_PATH = ""
+# IMAGENET_FID_NPZ = "/path/to/imagenet_256_fid_stats.npz"
+# IMAGENET_PR_NPZ = "/path/to/imagenet_val_prc_arr0.npz"
+
+# HF_REPO_ID = "Goodeat/drifting"
+# HF_ROOT = os.environ.get("HF_ROOT", "/path/to/hf_cache")
+
+IMAGENET_PATH = "/playpen-shared/dinara/datasets/imagenet"
 IMAGENET_CACHE_PATH = ""
-IMAGENET_FID_NPZ = "/path/to/imagenet_256_fid_stats.npz"
-IMAGENET_PR_NPZ = "/path/to/imagenet_val_prc_arr0.npz"
+IMAGENET_FID_NPZ = "/playpen-shared/dinara/datasets/imagenet/stats/imagenet_256_fid_stats.npz"
+IMAGENET_PR_NPZ = "/playpen-shared/dinara/datasets/imagenet/stats/imagenet_val_prc_arr0.npz"
 
 HF_REPO_ID = "Goodeat/drifting"
-HF_ROOT = os.environ.get("HF_ROOT", "/path/to/hf_cache")
+HF_ROOT = os.environ.get("HF_ROOT", "/playpen-shared/dinara/.hf-cache/hub")
