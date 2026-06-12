be68ed8e6cb5c8ed6bc58d4284fa6871b2462c6c
Dinara Aliyeva <dinara.ali@gmail.com>
2026-06-12 09:42:05 -0400

    update correct fid and latent_cache folders

--- utils/env.py
+++ utils/env.py
@@ -13,9 +13,9 @@ import os
 # HF_ROOT = os.environ.get("HF_ROOT", "/path/to/hf_cache")
 
 IMAGENET_PATH = "/playpen-shared/dinara/datasets/imagenet"
-IMAGENET_CACHE_PATH = ""
-IMAGENET_FID_NPZ = "/playpen-shared/dinara/datasets/imagenet/stats/imagenet_256_fid_stats.npz"
-IMAGENET_PR_NPZ = "/playpen-shared/dinara/datasets/imagenet/stats/imagenet_val_prc_arr0.npz"
+IMAGENET_CACHE_PATH = "/playpen-shared/dinara/datasets/imagenet/latent_cache"
+IMAGENET_FID_NPZ = "/playpen-shared/dinara/datasets/imagenet/FID_stats/imagenet_256_fid_stats.npz"
+IMAGENET_PR_NPZ = "/playpen-shared/dinara/datasets/imagenet/FID_stats/imagenet_val_prc_arr0.npz"
 
 HF_REPO_ID = "Goodeat/drifting"
 HF_ROOT = os.environ.get("HF_ROOT", "/playpen-shared/dinara/.hf-cache/hub")
